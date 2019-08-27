8b21123fe262088a05256495353341cb03506500
meishenmehaodemingle <407091625@qq.com>
2019-08-27 20:59:54 +0800

    完善了登录注册

--- src/main/java/com/vip/items/controller/RegisterController.java
+++ src/main/java/com/vip/items/controller/RegisterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Date;
+
 
 @RestController
 @Slf4j
